4d414b5ddcfb9d509f7ff6d45fd729a6452f9e97
Jessica Atkinson <jesslynn1007@gmail.com>
2022-08-19 09:14:42 -0400

    initial commit

---
+++ src/Main/Java/Account.java
@@ -0,0 +1,32 @@
+package Main.Java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Account
+{
+    //create an object to hold all the information needed for a new account
+
+    private String accountID;
+    //create date account was requested
+    //status of account (pushed, processing, no start, transitioned)
+    //first run date for account
+    //base price for account
+    //onboarding rep
+    //sales rep for account
+    //notes
+    //
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void bl()
+    {
+     System.out.println("Enter Anything");
+
+        List<String> test = new ArrayList<>();
+        test.add(scanner.nextLine());
+    }
+
+    //different ways
+}
---
+++ src/Main/Java/Main.java
@@ -0,0 +1,19 @@
+package Main.Java;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        //need to prompt user input to create a new account
+        //have multiple .txt files to house the various statuses of accounts, onboarding, different months, etc
+        //have a way to view only your accounts
+
+    }
+
+}
